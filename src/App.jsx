40ad824d01a939580b2c2219b19345720650ba10
Your Name <you@example.com>
2024-08-04 00:39:01 +0530

    Work compnent commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Nav from './components/Nav'
+import Work from './components/Work'
 
 const App = () => {
   return (
     <div className='bg-[#000000] h-screen w-full'>
       <Nav/>
+      <Work/>
     </div>
   )
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,12 @@
+
+const Work = () => {
+  return (
+    <div className="max-w-screen-xl mx-auto realtive text-center">
+        <h1 className=" text-[31vw] text-zinc-100 font-medium leading-tight">
+            Work
+        </h1>
+    </div>
+  )
+}
+
+export default Work
